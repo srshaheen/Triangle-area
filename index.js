c1a83625efd83c0f787e3d865f51b7fcb16a2516
Shahinur Rahman Shahin <srshahin111@gmail.com>
2022-11-26 10:24:05 +0600

    Added leap year or not function

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ const area = Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
 const p = document.getElementById('demo');
 p.innerHTML = area.toFixed(2);
 
+//leap year or not
+const demo2 = document.getElementById('demo2');
+const year = Number(prompt("Enter the year: "));
+if(year % 4 == 0 && year % 100 != 0){
+    demo2.innerHTML = (`${year} is leap year`);
+}
+else if(year % 400 == 0){
+    demo2.innerHTML = (`${year} is leap year`);
+}
+else{
+    demo2.innerHTML = (`${year} is not leap year`);
+}
+
